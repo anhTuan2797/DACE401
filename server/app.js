aa7275a87b2f0c8354a2be2f83c39f9775d0081e
anhtuan2797 <anhtuanug2797@gmail.com>
2020-12-09 10:51:27 +0700

    AnhTuan: them backend login page

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { send } = require('process');
 
 var app = express();
 
@@ -19,6 +23,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public','web_app')));
 
+// database connection setup
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'schooldb'
+});
+
+connection.connect((error)=>{
+  if(error){
+    return(console.error('error: '+error.message))
+  }
+  else{
+    console.log('connection success');
+  }
+});
+
+// session set up
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
@@ -26,7 +59,39 @@ app.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname,'/public/web_app','login_page.html'));
 });
 
+app.get('/main', (req,res)=>{
+  if(req.session.loggedIn){
+    res.send('test success'+req.session.userId);
+  }
+  else{
+    res.redirect('/');
+  }
+  res.end();
+});
 
+
+app.post('/login',(req,res)=>{
+  var userId = req.body.userId;
+  var password = req.body.password;
+  if(userId && password) {
+    connection.query('SELECT* FROM user_tbl WHERE user_id = ? AND user_password = ?',[userId,password],(error,results,field)=>{
+      if(error){
+        console.error(error);
+      } 
+
+      else if(results.length > 0) {
+          req.session.loggedIn = true;
+          req.session.userId = userId;
+          res.redirect('/main');
+        }else{
+          res.redirect('/');
+        }
+        res.end();
+    });
+  }else{
+    res.redirect('/');
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const { patch } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('main_page.html',{root: __dirname});
+  // res.sendFile('main_page.html',{root: __dirname});
+  res.sendFile(patch.join(__dirname,'../public/web_app','login_page.html'));
 });
 
 module.exports = router;
